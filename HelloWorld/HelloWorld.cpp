bf1ceb57cb028d6ad62bdc3974e7c0766f7fd2c4
Koupi <user@ubuntu.(none)>
2016-10-08 17:59:54 +0300

    Fastcgi HellowWorld has been added.

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,36 @@
+nclude <fastcgi2/component.h>
+#include <fastcgi2/component_factory.h>
+#include <fastcgi2/handler.h>
+#include <fastcgi2/request.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HelloWorldClass : virtual public fastcgi::Component, virtual public fastcgi::Handler {
+
+public:
+        HelloWorldClass(fastcgi::ComponentContext *context) :
+                fastcgi::Component(context) {
+        }
+        virtual ~HelloWorldClass() {
+        }
+
+public:
+        virtual void onLoad() {
+        }
+        virtual void onUnload() {
+        }
+        virtual void handleRequest(fastcgi::Request *request, fastcgi::HandlerContext *context) {
+		
+		string str = "Hello, World!\n";
+		request->write(str.c_str(), str.length());
+                request->setHeader("BetON-Header", "Reply from HelloWorld");
+        }
+
+};
+
+FCGIDAEMON_REGISTER_FACTORIES_BEGIN()
+FCGIDAEMON_ADD_DEFAULT_FACTORY("simple_factory", HelloWorldClass)
+FCGIDAEMON_REGISTER_FACTORIES_END()
